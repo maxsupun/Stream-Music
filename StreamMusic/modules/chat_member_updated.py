5b14d6829718595be43fa78d3e79d7f493e7395a
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-08-26 21:12:11 -0700

    Update chat_member_updated.py

--- StreamMusic/modules/chat_member_updated.py
+++ StreamMusic/modules/chat_member_updated.py
@@ -23,7 +23,5 @@ async def chat_member_updated(_, chat_member_updated: ChatMemberUpdated):
             )
         ) if (
             (chat_member_updated.new_chat_member.user.id)
-            in admins[chat_member_updated.chat.id]
-        ) else (
-            admins[chat_member_updated.chat.id].remove(
-                chat_member_updated.new_chat_member.user.id,
+            admins[chat_member_updated.chat.id]
+        ) else None
